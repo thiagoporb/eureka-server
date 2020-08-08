09424f7050fcebbd221bbe97cc67f894f2c9f44c
Thiago Tenório <thiagoporb@hotmail.com>
2020-08-08 02:26:16 -0300

    feat: :sparkles: Adiciona spring-cloud-starter-bus-kafka em favor do github webhook

--- src/main/kotlin/com/example/discovery/EurekaServerApplication.kt
+++ src/main/kotlin/com/example/discovery/EurekaServerApplication.kt
@@ -2,8 +2,10 @@ package com.example.discovery
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
+@RefreshScope
 @EnableEurekaServer
 @SpringBootApplication
 class EurekaServerApplication
