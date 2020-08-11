70959f50fa11e672a57eabbf3f5a2062b7e861cc
Thiago Tenório <thiagoporb@hotmail.com>
2020-08-10 23:01:16 -0300

    feat: :scissors: Modifica configuração de atualização do arquivo de configuração webhook para yaml
    Signed-off-by: Thiago Tenório <thiagoporb@hotmail.com>

--- src/main/kotlin/com/example/discovery/EurekaServerApplication.kt
+++ src/main/kotlin/com/example/discovery/EurekaServerApplication.kt
@@ -2,10 +2,8 @@ package com.example.discovery
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
-@RefreshScope
 @EnableEurekaServer
 @SpringBootApplication
 class EurekaServerApplication
